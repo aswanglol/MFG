e1c2f23dd0294c60c475c5d7f11f666d11b63d2f
aswanglol <s259076@students.aie.edu>
2025-10-07 11:09:21 -0700

    vector2 stuff and utils

--- MathLibrary/Vector2.h
+++ MathLibrary/Vector2.h
@@ -29,46 +29,126 @@ namespace MathClasses
 		Vector2(const ::Vector2& RVector2) { x = RVector2.x; y = RVector2.y; }
 		operator::Vector2()const { return { x,y }; }
 #endif
-		float Magnitude() const { return sqrtf((x * x) + (y * y)); }
-		float Dot(const Vector2& rhs) const { return ((x * rhs.x) + (y * rhs.y)); }
-		static float Dot(const Vector2& first, const Vector2& second) { return first.Dot(second); }
-		Vector2 Perp() const { return { -y,x }; }
+		float Magnitude() const { 
+			return sqrtf((x * x) + (y * y)); 
+		}
+		float Dot(const Vector2& rhs) const { 
+			return ((x * rhs.x) + (y * rhs.y)); 
+		}
+		static float Dot(const Vector2& first, const Vector2& second) { 
+			return first.Dot(second); 
+		}
+		Vector2 Perp() const { 
+			return { -y,x }; 
+		}
 
-		void Normalise() { float mag = this->Magnitude(); x /= mag; y /= mag; }
-		void SafeNormalise() { if (this->Magnitude() != 0) { Normalise(); } return; }
-		Vector2 Normalised() const { Vector2 copy = *this; copy.Normalise(); return copy; }
-		Vector2 SafeNormalised() const { if (this->Magnitude() != 0) { Vector2 copy = *this; copy.SafeNormalise(); return copy; }
+		void Normalise() { 
+			float mag = this->Magnitude(); x /= mag; y /= mag; 
+		}
+		void SafeNormalise() { 
+			if (this->Magnitude() != 0) { Normalise(); } return; 
+		}
+		Vector2 Normalised() const { 
+			Vector2 copy = *this; copy.Normalise(); return copy; 
+		}
+		Vector2 SafeNormalised() const {
+			if (this->Magnitude() != 0) { Vector2 copy = *this; copy.SafeNormalise(); return copy; }
+		}
+		Vector2 operator +(const Vector2& rhs) const {
+			Vector2 vector = { x, y };
+			vector.x += rhs.x;
+			vector.y += rhs.y;
+			return vector;
+		}
+		Vector2& operator +=(const Vector2& rhs) {
+			x += rhs.x;
+			y += rhs.y;
+			return *this;
+		}
+		Vector2 operator -(const Vector2& rhs) const {
+			Vector2 vector = { x, y };
+			vector.x -= rhs.x;
+			vector.y -= rhs.y;
+			return vector;
+		}
+		Vector2& operator -=(const Vector2& rhs) {
+			x -= rhs.x;
+			y -= rhs.y;
+			return *this;
+		}
+		Vector2 operator *(float rhs) const {
+			Vector2 vector = { x, y };
+			vector.x *= rhs;
+			vector.y *= rhs;
+			return vector;
+		}
+		Vector2& operator *=(float rhs) {
+			x *= rhs; y *= rhs; return *this;
+		}
 
-
-	//	Vector2 operator +(const Vector2& rhs) const;
-	//	Vector2& operator +=(const Vector2& rhs);
-	//	Vector2 operator -(const Vector2& rhs) const;
-	//	Vector2& operator -=(const Vector2& rhs);
-	//	Vector2 operator *(float rhs) const;
-	//	Vector2& operator *=(float rhs);
-	//	Vector2 operator /(float rhs) const;
-	//	Vector2& operator /=(float rhs);
-	//	bool operator == (const Vector2& rhs) const;
-	//	bool operator != (const Vector2& rhs) const;
-		bool Equals(const Vector2 & rhs, float Tolerance = MAX_FLOAT_DELTA) const { Vector2 distance =  }
-	//	std::string ToString() const;
-	//	operator float* ();
-	//	operator const float* () const;
+		Vector2 operator /(float rhs) const {
+			Vector2 vector = { x, y };
+			vector.x /= rhs;
+			vector.y /= rhs;
+			return vector;
+		}
+		Vector2& operator /=(float rhs) {
+			x /= rhs; y /= rhs; return *this;
+		}
+		bool operator == (const Vector2& rhs) const {
+			return Equals(rhs);
+		}
+		bool operator != (const Vector2 & rhs) const {
+			return !(Equals(rhs));
+		}
+		bool Equals(const Vector2 & rhs, float Tolerance = MAX_FLOAT_DELTA) const { 
+			Vector2 distance = { x - rhs.x, y - rhs.y };
+			distance.Absolute();
+		}
+		std::string ToString() const {
+			return "x: " + std::to_string(x) + ", y: " + std::to_string(y);
+		}
+		operator float* () {
+			return data;
+		}
+		operator const float* () const {
+			return data;
+		}
 	//	Vector2 operator *(const Vector2& rhs) const; rhs.y);
 	//	Vector2& operator *=(const Vector2& rhs);
 	//	Vector2 operator /(const Vector2& rhs) const;
 	//	Vector2& operator /=(const Vector2& rhs);
 	//	Vector2 operator -() const;
-	//	float& operator [](int dim);
-	//	const float& operator [](int dim) const;
-		float MagnitudeSqr() const { return ((x * x) + (y * y)); }
-	//	float Distance(const Vector2& other) const;
-	//	float DistanceSqr(const Vector2& other) const;
-	//	static float Distance(const Vector2& start, const Vector2& end);
-	//	static float DistanceSqr(const Vector2& start, const Vector2& end);
-	//	float AngleBetween(const Vector2& other) const;
-	//	inline Vector2 operator*(float scalar, const Vector2& vector);
+		float& operator [](int dim) {
+			return data[dim];
+		}
+		const float& operator [](int dim) const {
+			return data[dim];
+		}
+		float MagnitudeSqr() const { 
+			return ((x * x) + (y * y)); 
+		}
+		float Distance(const Vector2& other) const {
+			return (*this - other).Magnitude();
+		}
+		float DistanceSqr(const Vector2& other) const {
+			return (*this - other).MagnitudeSqr();
+		}
+		static float Distance(const Vector2& start, const Vector2& end) {
+			return start.Distance(end);
+		}
+		static float DistanceSqr(const Vector2& start, const Vector2& end) {
+			return start.DistanceSqr(end);
+		}
+		float AngleBetween(const Vector2& other) const {
+			return std::atan2f(y - other.y, x - other.x);
+		}
 
 
 	};
+	
+	inline Vector2 operator*(float scalar, const Vector2 & vector) {
+		return vector * scalar;
+	}
+
 }
